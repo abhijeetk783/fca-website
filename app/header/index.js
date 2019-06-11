72f074bd2cefc46ffaf089faedf889e3b23b12a2
rupesx26 <rupesx26@gmail.com>
2019-06-12 00:35:48 +0530

    home page setup

---
+++ app/header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Header extends Component {
+    //state = {  }
+    render() { 
+        return ( 
+            <div className='page-header'>
+                <header>
+                    <div className='logo'></div>
+                    <div className='hamburger'></div>
+                </header>
+            </div>
+         );
+    }
+}
+ 
+export default Header;
---
+++ app/home/Banner.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class Banner extends Component {
+    //state = {  }
+    render() { 
+        return ( 
+            <section className='banner'>
+                <div className='sub-title'>We are FINDCreative Ave.</div>
+                <div className='title'>to create is to unite</div>
+            </section>
+         );
+    }
+}
+ 
+export default Banner;
