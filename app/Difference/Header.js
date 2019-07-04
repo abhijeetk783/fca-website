7ec535f22fd3aa1684a24122c7f8f014df00fa8d
Rupesh Mayekar <rupesh.mayekar@bookmyshow.com>
2019-07-04 20:54:52 +0530

    restucture

--- app/Difference/Header.js
+++ app/Difference/Header.js
@@ -3,15 +3,20 @@ class PageHeader extends Component {
     render(){
         return(
             <section className='header'>
-                <div className='title'>We keep it simple</div>
-                <div className='details'>
-                    <div>The first step to solving a problem is to simplify the brief.</div>
-                    <div>Which is why, each project is headed by a Creative lead, who</div>
-                    <div>helps decode the brief and identify the right direction. Once</div>
-                    <div>the direction is set, a domain expert helps to understand and</div>
-                    <div>manage the project. All along the way, our process is a</div>
-                    <div>dialogue, so you too are part of it.</div>
+                <div className='container'>
+                    <div className='col-md-8'>
+                    <div className='title'>We keep it simple</div>
+                        <div className='details'>
+                            <div>The first step to solving a problem is to simplify the brief.</div>
+                            <div>Which is why, each project is headed by a Creative lead, who</div>
+                            <div>helps decode the brief and identify the right direction. Once</div>
+                            <div>the direction is set, a domain expert helps to understand and</div>
+                            <div>manage the project. All along the way, our process is a</div>
+                            <div>dialogue, so you too are part of it.</div>
+                        </div>
+                    </div>
                 </div>
+               
             </section>
         );
     }
